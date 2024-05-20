2b4af9129d0f66fccac99014dd6f930f30819665
James Slava Gaboriault-Whitcomb <jamesgab@x1000c0s0b0n1h0.chn.perlmutter.nersc.gov>
2024-05-20 14:51:38 -0700

    update

--- read.py
+++ read.py
@@ -8,16 +8,34 @@ import pprint
 from scipy import stats
 #matplotlib.matplotlib_fname()
 
-"""
-    GPU_TIME    WALL_TIME
-METRIC 0.09         0.09
-EVAL
-BASH
+# Convert a given csv file into an excel file with columns "WALL_TIME and GPU_TIME"
+def csv_to_excel(input_name, output_name):
+    convert_frame(input_name).to_excel(output_name)
 
-"""
+def plot_csv(input_name):
+    pass
 
-MEAN_COLUMN_INDEX = -2
+# Return a new dataframe with columns "WALL_TIME and GPU_TIME"
+def convert_frame(input_name):
+    dataframe = pd.read_csv(input_name)
+    dataframe = dataframe.set_index("event_id")
 
-dataframe = pd.read_csv("csv/results_final.csv")
-dataframe = dataframe.set_index("event_id")
-print(dataframe.loc["mean"])
+    # Fetch the mean values row
+    mean_series = dataframe.loc["mean"]
+    std_series = dataframe.loc["std"]
+
+    labels = ["Metric_Learning", "Build_Graph", "Filtering", "Preprocess", "InteractionGNN", "ccInfer"]
+    wall_time = []
+    gpu_time = []
+
+    for name, value in mean_series.items():
+        if name.endswith("gpu_time"):
+            gpu_time.append(f"{str(round(value, 4))} \u00B1 {str(round(std_series[name],4))}")
+        elif name.endswith("time"):
+            wall_time.append(f"{str(round(value, 4))} \u00B1 {str(round(std_series[name],4))}")
+
+    # Construct the new dataframe
+    data = {'':labels, "Wall_Time": wall_time, "GPU_Time": gpu_time}
+    return pd.DataFrame(data)
+
+csv_to_excel("csv/results_final.csv", "excel/results_final.xlsx")
